21cc4e09e36f7e38b14e88fa1195c1d1afe3c2fd
sijibomi <gbemilesanmi@gmail.com>
2023-11-09 01:49:56 +0100

    docker stuff

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
-import Express  from "express"
+import Express, { Request, Response } from "express"
 
 const app = Express()
 
-app.get("/" , (res, req) => {
+app.get("/", (_req: Request, res: Response) => {
     res.send("<h2>We are online bitches</h2>")
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.clear();
+    console.log("Server is listening on PORT:", PORT);
 })
